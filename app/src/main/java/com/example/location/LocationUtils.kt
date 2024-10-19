8b8064f60da098391679750cc103166b9b9a0393
m4DARA <king_mola@yahoo.com>
2024-10-19 23:25:22 +0300

    location

---
+++ app/src/main/java/com/example/location/LocationUtils.kt
@@ -0,0 +1,28 @@
+package com.example.location
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+
+class LocationUtils(val context:Context) {
+
+    fun HasLocationPermission(context: Context): Boolean {
+
+        return ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED
+                &&
+                ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED
+
+
+
+
+
+    }
+
+
+
+}
--- app/src/main/java/com/example/location/MainActivity.kt
+++ app/src/main/java/com/example/location/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.location
 
+import android.Manifest
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.magnifier
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.location.ui.theme.LocationTheme
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,17 +39,58 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LocationDisplay(
+    locationUtils: LocationUtils,
+    context: Context) {
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
+        onResult = { permissions ->
+            if (permissions [Manifest.permission.ACCESS_COARSE_LOCATION ] == true
+                && permissions [ Manifest.permission.ACCESS_FINE_LOCATION ] == true
+            ) {
+                //I Have Location
+            } else {
+                //I Don't Have Location
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LocationTheme {
-        Greeting("Android")
+            }
+        })
+
+    Column(modifier= Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        Text(text = "Location Not Available")
+        Button(onClick = {
+            if (locationUtils.hasLocationPermission(context)){
+
+
+            }else{
+
+
+            }
+
+
+
+            
+        }) {
+            Text(text = "Get Location")
+            
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
